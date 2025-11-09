b6c03aeda7baaaa0ad50537c21033a542cc0c0fa
SirBlessing <olaniyiolawale014@gmail.com>
2025-11-09 14:22:38 +0100

    added an#

--- backend/db.js
+++ backend/db.js
@@ -1,14 +1,16 @@
+
+
 import pkg from 'pg';
-const { Pool } = pkg;
 import dotenv from 'dotenv';
 dotenv.config();
 
+const { Pool } = pkg;
+
 const pool = new Pool({
-  user: process.env.PGUSER,
-  host: process.env.PGHOST,
-  database: process.env.PGDATABASE,
-  password: process.env.PGPASSWORD,
-  port: process.env.PGPORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
